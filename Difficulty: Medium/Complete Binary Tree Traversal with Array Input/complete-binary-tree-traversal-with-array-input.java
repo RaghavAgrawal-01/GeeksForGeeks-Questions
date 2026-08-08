2cc52582ad08fe939b9293aa7068870218b76793
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-08-08 14:55:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Complete Binary Tree Traversal with Array Input/complete-binary-tree-traversal-with-array-input.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelSort(int[] arr) {
+        // code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+        int idx = 0;
+        int levelSize = 1;
+        while(idx < n) 
+        {
+            ArrayList<Integer> level = new ArrayList<>();
+            for(int i = 0; i < levelSize && idx < n; i++) 
+            {
+                level.add(arr[idx++]);
+            }
+            Collections.sort(level);
+            result.add(level);
+            levelSize *= 2;
+        }
+        return result;
+    }
+}
