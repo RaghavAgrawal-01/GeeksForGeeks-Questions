36eec038aa92b2dbd7bde4a07a6f7fee7a9072bb
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-08-13 00:03:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Adventure in a Maze/adventure-in-a-maze.java
+++ Difficulty: Hard/Adventure in a Maze/adventure-in-a-maze.java
@@ -2,46 +2,39 @@ class Solution {
 
     public ArrayList<Integer> findWays(int[][] grid) {
         int n = grid.length;
-        final int MOD = 1_000_000_007;
+        int MOD = 1000000007;
 
         long[][] ways = new long[n][n];
         int[][] max = new int[n][n];
 
-        // -1 means unreachable
-        for (int i = 0; i < n; i++) {
-            java.util.Arrays.fill(max[i], -1);
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(max[i], -1);
         }
 
         ways[0][0] = 1;
         max[0][0] = grid[0][0];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
 
-                if (i == 0 && j == 0)
+                if(i == 0 && j == 0)
                     continue;
 
-                // Come from top: top cell must allow DOWN
-                if (i > 0 && (grid[i - 1][j] == 2 || grid[i - 1][j] == 3)) {
+                if(i > 0 && (grid[i - 1][j] == 2 || grid[i - 1][j] == 3)) {
                     ways[i][j] = (ways[i][j] + ways[i - 1][j]) % MOD;
 
-                    if (max[i - 1][j] != -1) {
-                        max[i][j] = Math.max(
-                            max[i][j],
-                            max[i - 1][j] + grid[i][j]
-                        );
+                    if(max[i - 1][j] != -1) {
+                        max[i][j] = Math.max(max[i][j],
+                            max[i - 1][j] + grid[i][j]);
                     }
                 }
 
-                // Come from left: left cell must allow RIGHT
-                if (j > 0 && (grid[i][j - 1] == 1 || grid[i][j - 1] == 3)) {
+                if(j > 0 && (grid[i][j - 1] == 1 || grid[i][j - 1] == 3)) {
                     ways[i][j] = (ways[i][j] + ways[i][j - 1]) % MOD;
 
-                    if (max[i][j - 1] != -1) {
-                        max[i][j] = Math.max(
-                            max[i][j],
-                            max[i][j - 1] + grid[i][j]
-                        );
+                    if(max[i][j - 1] != -1) {
+                        max[i][j] = Math.max(max[i][j],
+                            max[i][j - 1] + grid[i][j]);
                     }
                 }
             }
@@ -50,8 +43,6 @@ class Solution {
         ArrayList<Integer> ans = new ArrayList<>();
 
         ans.add((int) ways[n - 1][n - 1]);
-
-        // No valid path => maximum Adventure is 0
         ans.add(max[n - 1][n - 1] == -1 ? 0 : max[n - 1][n - 1]);
 
         return ans;
