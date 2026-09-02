10dce9165c1b31b51ea0c4024ef6bc8457a8f44b
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-09-02 13:01:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.java
@@ -0,0 +1,18 @@
+class Solution {
+    List<Integer> makeBeautiful(int[] arr) {
+        // code here
+        List<Integer> stack = new ArrayList<>();
+        for(int num : arr)
+        {
+            if(!stack.isEmpty() && ((stack.get(stack.size() - 1) >= 0 && num < 0) || (stack.get(stack.size() - 1) < 0 && num >= 0)))
+            {
+                stack.remove(stack.size() - 1);
+            }
+            else
+            {
+                stack.add(num);
+            }
+        }
+        return stack;
+    }
+}
