4f05c91fb6928f28e2ff4fd5bcdeab93fbe1174a
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-09-06 11:21:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+        while(i < a.length && j < b.length && k < c.length)
+        {
+            if(a[i] == b[j] && b[j] == c[k])
+            {
+                if(res.isEmpty() || res.get(res.size() - 1) != a[i])
+                {
+                    res.add(a[i]);
+                }
+                i++;
+                j++;
+                k++;
+            }
+            else if (a[i] < b[j]) {
+                i++;
+            }
+            else if (b[j] < c[k]) {
+                j++;
+            }
+            else {
+                k++;
+            }
+        }
+        return res;
+    }
+}
