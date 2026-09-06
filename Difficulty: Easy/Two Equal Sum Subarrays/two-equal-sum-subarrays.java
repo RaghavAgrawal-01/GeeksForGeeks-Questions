7278eb6bbd1e9d3617187e108e2cf25f8649d511
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-09-06 11:13:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Equal Sum Subarrays/two-equal-sum-subarrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canSplit(int arr[]) {
+        // code here
+        int total = 0;
+        for(int num : arr)
+        {
+            total += num;
+        }
+        if(total % 2 != 0) return false;
+        int prefixSum = 0;
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            prefixSum += arr[i];
+            if(prefixSum == total / 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
