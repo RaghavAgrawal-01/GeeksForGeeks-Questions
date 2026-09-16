453c9469cab350932f7b4779c11ab3bead62378e
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-09-16 16:29:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing Element in Range/missing-element-in-range.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        // code here
+        boolean[] present = new boolean[high - low + 1];
+        for(int x : arr)
+        {
+            if (x >= low && x <= high) present[x - low] = true;
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i <= high - low; i++)
+        {
+            if(!present[i]) ans.add(low + i);
+        }
+        return ans;
+    }
+}
