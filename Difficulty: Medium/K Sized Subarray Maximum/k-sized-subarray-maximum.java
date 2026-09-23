d076fa94f7ac883b7d2a558031f297d577a2ec39
Raghav Agrawal <raghavgarg34234@gmail.com>
2026-09-23 16:19:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -4,14 +4,17 @@ class Solution {
         Deque<Integer> deque = new LinkedList<>();
         for(int i = 0; i < arr.length; i++)
         {
-            while(!deque.isEmpty() && deque.peek() < i - k + 1) {
+            while(!deque.isEmpty() && deque.peek() < i - k + 1)
+            {
                 deque.poll();
             }
-            while(!deque.isEmpty() && arr[deque.peekLast()] < arr[i]) {
+            while(!deque.isEmpty() && arr[deque.peekLast()] < arr[i])
+            {
                 deque.pollLast();
             }
             deque.offer(i);
-            if(i >= k - 1) {
+            if(i >= k - 1)
+            {
                 result.add(arr[deque.peek()]);
             }
         }
